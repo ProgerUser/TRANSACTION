4eb01c3aea22d09627033718d3a0669ba7856b31
saidp <saidp@WIN7X64-ADM-06.sbra.com>
2019-10-19 20:17:22 +0300

    _

--- TRANSACTIONS/src/sample/Main.java
+++ TRANSACTIONS/src/sample/Main.java
@@ -41,13 +41,13 @@ public class Main extends Application {
 		// 3) Display the EmployeeOperations View
 		// showEmployeeView();
 
-		// Enter();
+		Enter();
 
-		Connect.connectionURL_ = "oradb-prm:1521/odb";
-		Connect.userID_ = "XXI";
-		Connect.userPassword_ = "ver8i";
+		// Connect.connectionURL_ = "oradb-prm:1521/odb";
+		// Connect.userID_ = "XXI";
+		// Connect.userPassword_ = "xxi";
 
-		showFirst();
+		// showFirst();
 	}
 
 	// Initializes the root layout.
