db57b75440d05ef837bb3853c681606304298ca3
ProgerUser <said.pachuliya@mail.ru>
2020-06-29 17:49:40 +0300

    11

--- TRANSACTIONS/src/sb_tr/controller/Ibank.java
+++ TRANSACTIONS/src/sb_tr/controller/Ibank.java
@@ -50,6 +50,8 @@ import java.util.concurrent.Executors;
 
 import org.controlsfx.control.table.TableFilter;
 
+import com.sun.rowset.CachedRowSetImpl;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -87,6 +89,8 @@ public class Ibank {
 	@FXML
 	private TextField db;
 
+	public Connection conn = null;
+
 	public void altert(String mess) {
 		Alert alert = new Alert(Alert.AlertType.INFORMATION);
 		Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
@@ -95,6 +99,31 @@ public class Ibank {
 		alert.setHeaderText(null);
 		alert.setContentText(mess);
 		alert.showAndWait();
+	}
+
+	public void dbConnect() {
+		try {
+			@SuppressWarnings("unused")
+			Connect con = new Connect();
+
+			// Setting Oracle JDBC Driver
+			Class.forName("oracle.jdbc.OracleDriver");
+
+			System.out.println("Драйвер Oracle JDBC зарегистрирован!");
+
+			// Establish the Oracle Connection using Connection String
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:" + login.getText() + "/" + password.getText() + "@" + db.getText() + "");
+
+		} catch (SQLException | ClassNotFoundException e) {
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+			stage.getIcons().add(new Image("terminal.png"));
+			alert.setTitle("Внимание");
+			alert.setHeaderText(null);
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		}
 	}
 
 	@FXML
@@ -108,29 +137,16 @@ public class Ibank {
 				try {
 					String acc_ = "";
 					int id = CLI.getSelectionModel().getSelectedItem().get_CLIENT_ID();
-					Connection conn = DBUtil.conn;
-					/*
-					Connection conn = DriverManager.getConnection("jdbc:oracle:thin:" + Connect.userID_ + "/"
-							+ Connect.userPassword_ + "@" + Connect.connectionURL_ + "");
-							*/
 					Statement sqlStatement = conn.createStatement();
-					String readRecordSQL = "select filtering\n" + 
-							"  from (select '[224100017]' filtering\n" + 
-							"          from dual\n" + 
-							"        union all\n" + 
-							"        select ACCOUNT filtering\n" + 
-							"          from ibank2.ACCOUNTS t\n" + 
-							"         where ID in\n" + 
-							"               (select ACCOUNT_ID from ibank2.C2ACCOUNTS t where client_id = "+id+"))\n" + 
-							" order by case\n" + 
-							"            when substr(filtering, 1, 1) = '[' then\n" + 
-							"             1\n" + 
-							"            else\n" + 
-							"             2\n" + 
-							"          end";
+					String readRecordSQL = "select filtering\n" + "  from (select '[224100017]' filtering\n"
+							+ "          from dual\n" + "        union all\n" + "        select ACCOUNT filtering\n"
+							+ "          from ibank2.ACCOUNTS t\n" + "         where ID in\n"
+							+ "               (select ACCOUNT_ID from ibank2.C2ACCOUNTS t where client_id = " + id
+							+ "))\n" + " order by case\n" + "            when substr(filtering, 1, 1) = '[' then\n"
+							+ "             1\n" + "            else\n" + "             2\n" + "          end";
 					ResultSet myResultSet = sqlStatement.executeQuery(readRecordSQL);
 					while (myResultSet.next()) {
-						acc_ = acc_+"\r\n"+myResultSet.getString("FILTERING");
+						acc_ = acc_ + "\r\n" + myResultSet.getString("FILTERING");
 					}
 					acc.setText(acc_);
 				} catch (Exception e) {
@@ -173,10 +189,8 @@ public class Ibank {
 		if (login.getText().length() == 0 | password.getText().length() == 0 | db.getText().length() == 0) {
 			altert("Заполните поля!");
 		} else {
-			Connect.connectionURL_ = db.getText();
-			Connect.userID_ = login.getText();
-			Connect.userPassword_ = password.getText();
-			ObservableList<Ibank2> empData = TerminalDAO.CLIENTS(db.getText(), login.getText(), password.getText());
+			this.dbConnect();
+			ObservableList<Ibank2> empData = CLIENTS();
 			populate_cli(empData);
 			autoResizeColumns(CLI);
 			TableFilter.forTableView(CLI).apply();
@@ -186,5 +200,113 @@ public class Ibank {
 
 	private void populate_cli(ObservableList<Ibank2> trData) {
 		CLI.setItems(trData);
+	}
+
+	public ObservableList<Ibank2> CLIENTS() {
+		String selectStmt = "select CLIENT_ID, NAME_CLN\n" + "  from ibank2.CLIENTS t";
+
+		// Execute SELECT statement
+
+		// Get ResultSet from dbExecuteQuery method
+		ResultSet rsEmps = dbExecuteQuery(selectStmt);
+
+		// Send ResultSet to the getEmployeeList method and get employee object
+		ObservableList<Ibank2> Cli_lst = get_cli(rsEmps);
+
+		// Return Cli_lst object
+		return Cli_lst;
+	}
+
+	private static ObservableList<Ibank2> get_cli(ResultSet rs) {
+		try {
+			ObservableList<Ibank2> user_in_list = FXCollections.observableArrayList();
+			while (rs.next()) {
+				Ibank2 user_in = new Ibank2();
+				user_in.set_CLIENT_ID(rs.getInt("CLIENT_ID"));
+				user_in.set_NAME_CLN(rs.getString("NAME_CLN"));
+				user_in_list.add(user_in);
+			}
+			return user_in_list;
+		} catch (SQLException e) {
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+			stage.getIcons().add(new Image("terminal.png"));
+			alert.setTitle("Внимание");
+			alert.setHeaderText(null);
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		}
+		return null;
+	}
+
+	// DB Execute Query Operation
+	public ResultSet dbExecuteQuery(String queryStmt) {
+		// Declare statement, resultSet and CachedResultSet as null
+		Statement stmt = null;
+		ResultSet resultSet = null;
+		CachedRowSetImpl crs = null;
+		try {
+			// Connect to DB (Establish Oracle Connection)
+			if (conn == null && !conn.isClosed()) {
+				this.dbConnect();
+			}
+			System.out.println("Выборка данных: " + queryStmt + "\n");
+
+			// Create statement
+			stmt = conn.createStatement();
+
+			// Execute select (query) operation
+			resultSet = stmt.executeQuery(queryStmt);
+
+			// CachedRowSet Implementation
+			// In order to prevent "java.sql.SQLRecoverableException: Closed
+			// Connection: next" error
+			// We are using CachedRowSet
+			crs = new CachedRowSetImpl();
+			crs.populate(resultSet);
+		} catch (SQLException e) {
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+			stage.getIcons().add(new Image("terminal.png"));
+			alert.setTitle("Внимание");
+			alert.setHeaderText(null);
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		} finally {
+			if (resultSet != null) {
+				// Close resultSet
+				try {
+					resultSet.close();
+				} catch (SQLException e) {
+					Alert alert = new Alert(Alert.AlertType.INFORMATION);
+					Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+					stage.getIcons().add(new Image("terminal.png"));
+					alert.setTitle("Внимание");
+					alert.setHeaderText(null);
+					alert.setContentText(e.getMessage());
+					alert.showAndWait();
+
+				}
+			}
+			if (stmt != null) {
+				// Close Statement
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					Alert alert = new Alert(Alert.AlertType.INFORMATION);
+					Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+					stage.getIcons().add(new Image("terminal.png"));
+					alert.setTitle("Внимание");
+					alert.setHeaderText(null);
+					alert.setContentText(e.getMessage());
+					alert.showAndWait();
+
+				}
+			}
+			// Close connection
+			// dbDisconnect();
+		}
+		// Return CachedRowSet
+		return crs;
 	}
 }
