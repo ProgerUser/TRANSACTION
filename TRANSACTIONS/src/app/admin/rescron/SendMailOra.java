5cd24209552d525f6bc05c27f34bebbb95e7805b
ProgerUser <said.pachuliya@mail.ru>
2021-10-03 16:22:47 +0300

    ++

--- TRANSACTIONS/src/app/admin/rescron/SendMailOra.java
+++ TRANSACTIONS/src/app/admin/rescron/SendMailOra.java
@@ -15,69 +15,74 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
 public class SendMailOra {
-	
-	public static Properties prop;
 
-	public static String SendMail(String subj,String msg,String mailto) {
-		String ret = "ok";
-		try {
-			// prop
-			prop = new Properties();
-			prop.put("mail.transport.protocol", "smtp");
-			prop.put("mail.smtp.starttls.enable", "true");
-			prop.put("mail.smtp.host", "mail.sbra.su");
-			prop.put("mail.smtp.port", "587");
-			prop.put("mail.smtp.auth", "true");
-			prop.put("mail.smtp.ssl.enable", "false");
-			prop.put("mail.smtp.ssl.trust", "mail.sbra.su");
-			prop.put("mail.smtp.user", "portal@sbra.su");
-			prop.put("mail.smtp.password", "Cjktyysq_098");
-			prop.put("mail.store.protocol", "imap");
-			prop.put("mail.imap.host", "mail.sbra.su");
-			prop.put("mail.imap.port", "993");
-			prop.put("mail.imap.auth", "true");
-			prop.put("mail.imap.ssl.enable", "false");
-			prop.put("mail.imap.ssl.trust", "mail.sbra.su");
-			prop.put("mail.imap.user", "portal@sbra.su");
-			prop.put("mail.imap.password", "Cjktyysq_098");
-			// session
-			Session session = Session.getInstance(prop, new Authenticator() {
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(prop.getProperty("mail.smtp.user"),
-							prop.getProperty("mail.smtp.password"));
-				}
-			});
-			Message message = new MimeMessage(session);
-			
-			message.setHeader("Content-Type", "text/plain; charset=UTF-8");
-			
-			message.setFrom(new InternetAddress(prop.getProperty("mail.smtp.user")));
-			
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailto));
-			
-			message.setSubject(subj);
+  public static Properties prop;
+
+  public static String SendMail(String subj,String msg,String mailto) {
+    String ret = "ok";
+    try {
+      // prop
+      prop = new Properties();
+      prop.put("mail.transport.protocol", "smtp");
+      prop.put("mail.smtp.starttls.enable", "true");
+      prop.put("mail.smtp.host", "mail.sbra.su");
+      prop.put("mail.smtp.port", "587");
+      prop.put("mail.smtp.auth", "true");
+      prop.put("mail.smtp.ssl.enable", "false");
+      prop.put("mail.smtp.ssl.trust", "mail.sbra.su");
+      prop.put("mail.smtp.user", "portal@sbra.su");
+      prop.put("mail.smtp.password", "Cjktyysq_098");
+      prop.put("mail.store.protocol", "imap");
+      prop.put("mail.imap.host", "mail.sbra.su");
+      prop.put("mail.imap.port", "993");
+      prop.put("mail.imap.auth", "true");
+      prop.put("mail.imap.ssl.enable", "false");
+      prop.put("mail.imap.ssl.trust", "mail.sbra.su");
+      prop.put("mail.imap.user", "portal@sbra.su");
+      prop.put("mail.imap.password", "Cjktyysq_098");
+      // session
+      Session session = Session.getInstance(prop, new Authenticator() {
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+          return new PasswordAuthentication(prop.getProperty("mail.smtp.user"),
+              prop.getProperty("mail.smtp.password"));
+        }
+      });
+      
+      Message message = new MimeMessage(session);
+
+      message.setHeader("Content-Type", "text/plain; charset=UTF-8");
+
+      message.setFrom(new InternetAddress(prop.getProperty("mail.smtp.user")));
+
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mailto));
+
+      message.setSubject(subj);
 
 
-			MimeBodyPart mimeBodyPart = new MimeBodyPart();
-			mimeBodyPart.setContent(msg, "text/plain; charset=UTF-8");
+      MimeBodyPart mimeBodyPart = new MimeBodyPart();
+      mimeBodyPart.setContent(msg, "text/plain; charset=UTF-8");
 
-			Multipart multipart = new MimeMultipart();
-			multipart.addBodyPart(mimeBodyPart);
+      Multipart multipart = new MimeMultipart();
+      multipart.addBodyPart(mimeBodyPart);
 
-			message.setContent(multipart);
+      message.setContent(multipart);
 
-			Transport.send(message);
-		} catch (Exception e) {
-			ret = getStackTrace(e);
-		}
-		return ret;
-	}
+      Transport.send(message);
+    } catch (Exception e) {
+      ret = getStackTrace(e);
+    }
+    return ret;
+  }
+  
+  public static void main(String[] args) {
+	  System.out.println(SendMail("123","123","pachuliya.s@sbra.su"));
+  }
 
-	public static String getStackTrace(final Throwable throwable) {
-		final StringWriter sw = new StringWriter();
-		final PrintWriter pw = new PrintWriter(sw, true);
-		throwable.printStackTrace(pw);
-		return sw.getBuffer().toString();
-	}
+  public static String getStackTrace(final Throwable throwable) {
+    final StringWriter sw = new StringWriter();
+    final PrintWriter pw = new PrintWriter(sw, true);
+    throwable.printStackTrace(pw);
+    return sw.getBuffer().toString();
+  }
 }
